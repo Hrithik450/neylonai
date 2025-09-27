492df388b0e5ae1155e183a64d54389e2c5181b4
Hruthik <mhrithik450@gmail.com>
2025-09-27 14:59:01 +0530

    exposed health api route to check the health of server

--- server/ai_solutionz/views.py
+++ server/ai_solutionz/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
 
-# Create your views here.
+@api_view(["GET"])
+def health_check(request: Request):
+    return Response({"status": "ok"})
