d6364c5571c5cb3059816532388ddc8d9312af31
Hruthik <mhrithik450@gmail.com>
2025-10-15 06:33:46 +0530

    scheduled cron job successfully

--- primary-server/text_generation/services/cron_job_service.py
+++ primary-server/text_generation/services/cron_job_service.py
@@ -1,6 +1,7 @@
 import traceback
 from typing import Optional
 from pydantic import BaseModel
+from ..models import User
 
 class CronJobResponse(BaseModel):
     success: bool
@@ -11,7 +12,9 @@ class CrobJobService:
     @classmethod
     def rest_daily_limit(cls):
         try:
-            print("Token reset successfull")
+            updated_count = User.objects.all().update(daily_limit=200)
+            print(f"Token reset successful for {updated_count} users")
+
             return CronJobResponse(success=True, error=None)
         except Exception as e:
             return CronJobResponse(success=False, error=f'Error occured: {str(e)}, details: {traceback.format_exc()}')
