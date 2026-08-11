21a687aa99d9e9cec9b904187ebcffbd4bb0b6bd
Hrithik450 <mhrithik450@gmail.com>
2026-08-12 00:57:09 +0530

    feat(ui): scaffold ui package

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,70 @@
+// Utilities
+export { cn } from "./lib/cn";
+export { shortTimeAgo } from "./lib/format";
+
+// Primitives
+export { Badge, badgeVariants } from "./components/badge";
+export { Button, buttonVariants } from "./components/button";
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from "./components/card";
+export {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  ChartLegend,
+  ChartLegendContent,
+  ChartStyle,
+  type ChartConfig,
+} from "./components/chart";
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "./components/form";
+export { Label } from "./components/label";
+export {
+  PromptInput,
+  PromptInputTextarea,
+  PromptInputActions,
+  PromptInputAction,
+  type PromptInputTextareaProps,
+} from "./components/prompt-input";
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "./components/select";
+export { Skeleton } from "./components/skeleton";
+export { Textarea } from "./components/textarea";
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "./components/tooltip";
+
+// Charts
+export { ChartAreaDefault } from "./components/charts/area-chart";
+export { ChartBarMultiple } from "./components/charts/bar-chart";
+export { ChartPieLabel } from "./components/charts/pie-chart";
+export { ChartRadarLegend } from "./components/charts/radar-chart";
+export { ChartRadialText } from "./components/charts/radial-chart";
