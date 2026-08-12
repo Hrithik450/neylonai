8dc9ba1078b1f206e461bc069229ce32daac4905
Hrithik450 <mhrithik450@gmail.com>
2026-08-12 06:18:12 +0530

    feat(web): add public assets, fonts, and sounds

---
+++ apps/web/src/assets/fonts.ts
@@ -0,0 +1,21 @@
+import localFont from "next/font/local";
+
+export const guminertMedium = localFont({
+  src: "./fonts/Guminert-Medium.ttf",
+  weight: "550",
+});
+
+export const guminertRegular = localFont({
+  src: "./fonts/Guminert-Regular.ttf",
+  weight: "400",
+});
+
+export const guminertBold = localFont({
+  src: "./fonts/Guminert-Bold.ttf",
+  weight: "700",
+});
+
+export const sfProRegular = localFont({
+  src: "./fonts/SF-Pro-Display-Regular.otf",
+  weight: "400",
+});
